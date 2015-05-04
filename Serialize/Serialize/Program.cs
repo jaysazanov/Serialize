ff00e586bf1bd8679e1f695d645c18055915e417
Jay Sazanov <jaysazanov@outlook.com>
2015-05-04 22:48:05 +0300

    Serialize

---
+++ Serialize/Serialize/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Serial
+    {
+        [XmlElement(Namespace = "http://planetakino.ua/odessa/ua/showtimes/xml/")]
+        public string ItemName;
+        [XmlElement(DataType = "movies")]
+        public string movies;
+        [XmlElement(DataType = "movie")]
+        public string movie;
+        [XmlElement(DataType = "title")]
+    } 
+}
